d415a509c17b86c074417594c91f3855e72f644b
madisonDavis7 <penny-davis-12@outlook.com>
2026-02-05 21:18:00 -0700

    added pages for the publications and reports buttons, added navigation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import PublicationsAndReports from './pages/PublicationsAndReports'
+import Spending from './pages/Spending'
+import Publications from './pages/Publications'
+import RecentFindings from './pages/RecentFindings'
+import News from './pages/News'
 
 export default function App() {
   return (
@@ -9,6 +13,10 @@ export default function App() {
         <Route path="/" element={<Home />} />
         {/* Add rest of the routes here */}
         <Route path="/PublicationsAndReports" element={<PublicationsAndReports />} />
+        <Route path="/Spending" element={<Spending />} />
+        <Route path="/News" element={<News />} />
+        <Route path="/RecentFindings" element={<RecentFindings />} />
+        <Route path="/Publications" element={<Publications />} />
 
       </Routes>
     </Router>
---
+++ web/src/pages/Spending.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import '../styles/Spending.css'
+
+export default function Spending() {
+    return (
+        <main className="spending-page">
+            <Header />
+            <h1>Spending Page</h1>
+            <Footer />
+        </main>
+    )
+}
