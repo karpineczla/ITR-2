8ebb76059f65f3acb2380230f445bdc6f9327bf6
Logan Karpinecz <loganandpeanut@gmail.com>
2026-02-09 14:17:06 -0700

    update before merge to main

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Hero from '../components/Hero'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import HomeGrid from '../components/HomeGrid'
-import HomeButtonCards from '../components/HomeButtonCards'
+import HomeButtonCards from '../components/homeButtonCards'
 import '../styles/Home.css'
 
 export default function Home() {
