d5d4ff30b548451d67b866650995efa265833e7e
Sydney Escobar <se176171@umconnect.umt.edu>
2026-02-16 11:06:02 -0700

    non breaking steps to using website preview, process issue in sanityClient.ts needs to be fixed

--- web/src/sanityClient.ts
+++ web/src/sanityClient.ts
@@ -1,9 +1,24 @@
 import { createClient } from '@sanity/client'
 
+declare global {
+  interface Window {
+    ENV: {
+      PUBLIC_SANITY_PROJECT_ID: string;
+      PUBLIC_SANITY_DATASET: string;
+      PUBLIC_SANITY_STUDIO_URL: string;
+    };
+  }
+}
+// Must fix process error
+const env = typeof document === "undefined" ? process.env : window.ENV; 
+
 export const client = createClient({
     projectId: 'a9qy1267',
     dataset: 'production',
-    apiVersion: '2023-01-01',
+    apiVersion: '2025-10-30',
     useCdn: true,
+    stega: {
+    studioUrl: env.PUBLIC_SANITY_STUDIO_URL,
+  },
 })
 
---
+++ web/src/session.ts
@@ -0,0 +1,35 @@
+import { createCookieSessionStorage } from "react-router";
+import type { loadQuery } from "@sanity/react-loader"
+
+const { getSession, commitSession, destroySession } =
+  createCookieSessionStorage({
+    cookie: {
+      httpOnly: true,
+      name: "__sanity_preview",
+      path: "/",
+      sameSite: !import.meta.env.DEV ? "none" : "lax",
+      // FIXME: Replace with hex generator not node.crypto
+      secrets: ['crypto.randomBytes(16).toString("hex")'],
+      secure: !import.meta.env.DEV,
+    },
+  });
+
+async function getPreviewData(request: Request): Promise<{
+  preview: boolean;
+  options: Parameters<typeof loadQuery>[2]
+}> {
+  const session = await getSession(request.headers.get("Cookie"));
+  const preview = session.get("previewMode") || false
+  return {
+    preview,
+    options: preview ? {
+      perspective: session.has("perspective") ? session.get("perspective").split(',') : "drafts",
+      stega: true,
+    } : {
+      perspective: 'published',
+      stega: false,
+    }
+  };
+}
+
+export { commitSession, destroySession, getSession, getPreviewData };
