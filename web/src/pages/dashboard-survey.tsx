66678991351c8130debf5b7dd2cc2d9a6768b05a
madisonDavis7 <penny-davis-12@outlook.com>
2026-03-08 08:50:38 -0600

    finished dashboard survey page, changed banner color for announcements

--- web/src/pages/dashboard-survey.tsx
+++ web/src/pages/dashboard-survey.tsx
@@ -1,15 +1,16 @@
 import Header from "../components/general/Header";
 import Footer from "../components/general/Footer";
 import TitleAndText from "../components/general/TitleAndText";
+import BackButton from "../components/general/BackButton";
 
 export default function DashboardSurveyPage() {
     return (
         <main className="dashboard-survey-page">
             <Header />
+            <BackButton to="/interactive-data" />
             <TitleAndText sectionKey="dashboard-survey-part-one" />
             <TitleAndText sectionKey="dashboard-survey-part-two" />
             <TitleAndText sectionKey="dashboard-survey-part-three" />
-
             <Footer />
         </main>
     )
